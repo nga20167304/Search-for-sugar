4e71eefc055692a6df4697c3b77aa071f3dbcaf3
Nga <38340962+nga20167304@users.noreply.github.com>
2022-02-11 09:18:17 +0700

    fix error when run github action

---
+++ spec/support/capybara.rb
@@ -0,0 +1,36 @@
+Capybara.register_driver :chrome_headless do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    'goog:chromeOptions': {
+      args: %w[ no-sandbox headless disable-gpu --window-size=1920,1080]
+    }
+  )
+  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+end
+
+#Ensure Log directory exists
+%x(mkdir -p tmp/selenium_logs)
+
+Capybara.register_driver :chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    'goog:chromeOptions': { args: %w[ start-maximized ] }
+  )
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: capabilities,
+    service: ::Selenium::WebDriver::Service.chrome(
+      args: {
+        log_path: "./tmp/selenium_logs/selenium-#{Time.now.to_i}.log",
+        verbose: true
+      }
+    )
+  )
+end
+
+Capybara.javascript_driver = ENV.fetch('CAPYBARA_JAVASCRIPT_DRIVER', :chrome).to_sym
+
+#must allow this URL if Chromedriver needs to download a binary
+WebMock.disable_net_connect!(allow: 'chromedriver.storage.googleapis.com', allow_localhost: true)
+
+Capybara.server = :webrick
