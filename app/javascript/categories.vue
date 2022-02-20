c7ddc1b427606dce2c036ad0b1ce2c57b1994396
Nga <38340962+nga20167304@users.noreply.github.com>
2022-02-20 18:47:26 +0700

    show foods of category

--- app/javascript/categories.vue
+++ app/javascript/categories.vue
@@ -1,14 +1,13 @@
 <template lang="pug">
   <div class="grid-item" v-for="category in categories">
-    <span><a v-bind:href="'/categories/'+ category.id" @click='showFoods(category.id)'> {{ category.name }} </a></span>
+    <span><a v-bind:href="'/categories/'+ category.id"> {{ category.name }} </a></span>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      categories: "",
-      foods: ""
+      categories: ""
     };
   },
   created() {
@@ -30,24 +29,7 @@ export default {
         .catch((error) => {
           console.warn("Failed to parsing", error);
         });
-      },
-    showFoods(id) {
-      fetch(`/categories/${id}.json`, {
-        method: "GET",
-      })
-        .then((response) => {
-          return response.json();
-        })
-        .then((json) => {
-          if (json) {
-            this.foods = json
-            console.log(this.foods)
-          }
-        })
-        .catch((error) => {
-          console.warn("Failed to parsing", error);
-        });
-    }
+      }
     }
   }
 </script>
